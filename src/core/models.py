e83de21ae968a77c4f7583124bfb6aa605098a9d
Humberto Rocha <humrochagf@gmail.com>
2012-11-07 14:50:01 -0200

    corrigindo model

--- src/core/models.py
+++ src/core/models.py
@@ -23,9 +23,9 @@ class Produto(models.Model):
     # se for NULL, significa que o produto "não possui estoque", o que é
     # diferente de um produto que "não possui nenhum item em estoque", caso no
     # qual estoque seria igual a zero.
-    tipo = models.SmallPositiverIntegerField(choices=TIPO_CHOICES)
+    tipo = models.PositiveSmallIntegerField(choices=TIPO_CHOICES)
     preco = DinheiroField(u"Preço")
-    titulo = models.CharField(u"Titulo", max_digits=255)
+    titulo = models.CharField(u"Titulo", max_length=255)
     estoque = models.PositiveIntegerField(u"Estoque", null=True)
 
     composicao = models.ManyToManyField('self')
@@ -45,17 +45,6 @@ class Tema(models.Model):
         return self.tema
 
 
-class Pedido(models.Model):
-    data_solicitacao = models.DateTimeField(u"Data de solicitação", auto_now_add=True)
-    data_saida = models.DateTimeField(u"Data de envio")
-    data_retorno = models.DateTimeField(u"Data de retorno", null=True)
-    anotacoes = models.TextField(u"Anotações")
-
-    produtos = models.ManyToManyField(Produto, related_name='pedidos')
-    # TODO checar referencia
-    descontos = models.ManyToManyField(Desconto, related_name='descontos', null=True)
-
-
 class Desconto(models.Model):
     # Classe criada para lançamentos de descontos/multas 
     # o desconto pode ser negativo ou positivo e relativo ou absoluto
@@ -68,10 +57,21 @@ class Desconto(models.Model):
         (3, u"Credito R$"),
     )
 
-    tipo = models.SmallPositiverIntegerField(choices=TIPO_CHOICES)
+    tipo = models.PositiveSmallIntegerField(choices=TIPO_CHOICES)
     valor_relativo = models.PositiveIntegerField(U"Valor %", null=True)
     valor_absoluto = DinheiroField(u"Valor R$", null=True)
     justificativa = models.TextField(u"Justificativa do desconto")
+
+
+class Pedido(models.Model):
+    data_solicitacao = models.DateTimeField(u"Data de solicitação", auto_now_add=True)
+    data_saida = models.DateTimeField(u"Data de envio")
+    data_retorno = models.DateTimeField(u"Data de retorno", null=True)
+    anotacoes = models.TextField(u"Anotações")
+
+    produtos = models.ManyToManyField(Produto, related_name='pedidos')
+    # TODO checar referencia
+    descontos = models.ManyToManyField(Desconto, related_name='descontos', null=True)
 
 
 class Pagamento(models.Model):
@@ -87,14 +87,14 @@ class Pagamento(models.Model):
 class PagamentoDebito(Pagamento):
     banco = models.CharField(max_length=255)
     numero = models.CharField(u"Número do cheque", max_length=255)
-    titular_conta = models.CharField(u"Titular da conta")
+    titular_conta = models.CharField(u"Titular da conta", max_length=255)
     data_deposito = models.DateTimeField(u"Data do depósito")
 
 
 class PagamentoCheque(Pagamento):
     banco = models.CharField(max_length=255)
     numero = models.CharField(u"Número do cheque", max_length=255)
-    titular_conta = models.CharField(u"Titular da conta")
+    titular_conta = models.CharField(u"Titular da conta", max_length=255)
 
 
 # TODO modelar o parceiro e dar um jeito de permitir que ele possa
