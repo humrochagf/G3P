a8d3b80d3edea4f52e3b5835aed3f14fa4ec631d
Humberto Rocha <humrochagf@gmail.com>
2012-11-08 15:29:31 -0200

    registrando modelos

--- src/core/admin.py
+++ src/core/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from src.core.models import *
 
 admin.site.register(Produto)
+admin.site.register(Pedido)
+admin.site.register(Desconto)
+admin.site.register(Pagamento)
