24b0e0cc5af7648542c80d305554ab9bb1a5b774
Humberto Rocha <humrochagf@gmail.com>
2012-11-15 20:55:20 -0200

    customizando produtos no admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from src.core.models import *
 
-admin.site.register(Produto)
 admin.site.register(Pedido)
 admin.site.register(Desconto)
 admin.site.register(Pagamento)
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    search_fields = ['titulo']
+    list_filter = ('tipo',)
+    filter_horizontal = ('composicao',)
+
+admin.site.register(Produto, ProdutoAdmin)
